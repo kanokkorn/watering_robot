11ba7dc896809ba9ca3edd9b4b8af97f8dd83948
Kanokkorn <kanokorn@outlook.jp>
2018-07-15 21:09:19 +0700

    add def main

--- rasp_agent.py
+++ rasp_agent.py
@@ -9,18 +9,20 @@ def read_loc(lines):
         lines = f.readlines()
     return(lines)
 def action(act):
-    if (lat_int == lat_fin) && (lon_int == lon_fin) :
+    if (lat_int == lat_fin) & (lon_int == lon_fin) :
         print("Agent is Watering")
         ser.write("Stop")
         time.sleep(10)
         pass
-    if  (lat_int != lat_fin) && (lon_int != lon_fin) :
+    if  (lat_int != lat_fin) & (lon_int != lon_fin) :
         print("Agent is moving")
         ser.write("Forward")
         time.sleep(3)
         pass
     
 def main(self, lines, act):
+    pass
+    
     
     
  
