19c38403c6eb7ce93aebcb411401bb6594560f9a
Kanokkorn <kanokorn@outlook.jp>
2018-08-05 23:57:16 +0900

    fix

--- gps_planner.py
+++ gps_planner.py
@@ -1,25 +1,25 @@
 import csv
 import math
+import time
 
 #get lat lon from Ublox neo m6 via UART
 def read_loc():
-    with open('lat_lon.csv') as f:
-        lines = f.readlines()
-        for line in lines:
-            print(line)
-    pass
-def haversine(lines):
+    with open('lat_lon.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            x = row[0]
+            y = row[1]
+            print(x, y)
+            time.sleep(60)    
+    return (x, y)
+def distance():
+    del_lat
     
-    lat_int = 1
-    lon_int = 0
-    lat_fin = 1
-    lon_fin = 0
 
-    return lat_int, lat_fin, lon_int, lon_fin
     
 def Deg_Rad(deg):
     rad = math.radians(deg)
+
     
     return rad
-    
-read_loc()    
+ read_loc()    
