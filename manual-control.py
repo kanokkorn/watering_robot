7d3da298ebedb5144e4187bb9ae8ea06ee98d856
Kanokkorn <kanokorn@outlook.jp>
2018-08-07 09:49:32 +0700

    fix

--- manual-control.py
+++ manual-control.py
@@ -1,1 +1,17 @@
-import 
+import serial
+import sys
+
+# global variable
+
+
+
+def forward():
+    return
+def backward():
+    return
+def letf():
+    return
+def right():
+    return
+def main()
+    if 
