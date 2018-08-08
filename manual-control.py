21521bae2b53b878707232e490fd650ccf719462
Kanokkorn <kanokorn@outlook.jp>
2018-08-08 22:12:12 +0900

    fix

--- manual-control.py
+++ manual-control.py
@@ -2,7 +2,7 @@ import serial
 import sys
 
 # global variable
-
+j_val = ()
 
 
 def forward():
@@ -13,5 +13,11 @@ def letf():
     return
 def right():
     return
-def main()
-    if 
+def main():
+    if j_val < 0 & j_val > -1:
+        return
+        
+    elif j_val < 0 & j_val > -1:
+        return
+    
+
