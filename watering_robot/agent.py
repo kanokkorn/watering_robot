2f0fdbe67b9ea5c5e655183f8651d2378cfde758
Kanokkorn <kanokorn@outlook.jp>
2018-08-15 11:58:29 +0700

    fix agent

--- watering_robot/agent.py
+++ watering_robot/agent.py
@@ -2,9 +2,22 @@
 import comm
 import gps_planner
 import time
+import threading as thrd
 
-def rotate():
+def rotate_left():
     comm.serial()
- 
-    
-    
+    return
+def rotate_right():
+    comm.serial()
+    return
+def forward():
+    comm.serial()
+    return
+def backward():
+    comm.serial()
+    return
+try:
+    thrd._start_new_thread()
+    pass
+except expression as identifier:
+    pass
