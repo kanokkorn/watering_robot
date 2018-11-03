4e6c06978291e9ce8e21ea570aa1bd0a36d32ba8
Kanokkorn <kanokorn@outlook.jp>
2018-11-03 17:33:34 +0700

    fix pynmea2 test

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -3,8 +3,9 @@ import pynmea2
 
 import serial
 ser = serial.Serial ("/dev/ttyS0")    #Open named port 
-ser.baudrate = 9600                     #Set baud rate to 9600
-data = ser.read(60)                     #Read ten characters from serial port to data
-msg = pynmea2.parse(data)
-print(msg.lat, msg.lat)                        #Send back the received data
+ser.baudrate = 9600
+data = ser.read(60)
+if data[0:6] == '$GPGGA':                     #Set baud rate t
+    msg = pynmea2.parse(data)
+    print(msg.lat, msg.lat)                        #Send back the received data
 ser.close() 
