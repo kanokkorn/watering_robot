4013698a7dfdac93035b0c0f2b81b0d2d5f98f2b
Kanokkorn <kanokorn@outlook.jp>
2018-11-03 17:38:09 +0700

    typo

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -6,9 +6,9 @@ ser.baudrate = 9600
 
 while 1:
 
-data = ser.read(60)
-if data[0:6] == '$GPGGA':                     #Set baud rate t
-    msg = pynmea2.parse(data)
-    print(msg.lat)
-    print(, msg.lat)                        #Send back the received data
-    ser.close() 
+    data = ser.read(60)
+    if data[0:6] == '$GPGGA':                     #Set baud rate t
+        msg = pynmea2.parse(data)
+        print(msg.lat)
+        print(, msg.lat)                        #Send back the received data
+        ser.close() 
