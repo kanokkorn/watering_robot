2d6bb222df08902b3c367037520269cde1d192c7
Kanokkorn <kanokorn@outlook.jp>
2018-11-03 18:03:55 +0700

    0.2 to 0.01

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -1,7 +1,7 @@
 import serial
 import pynmea2
 import time 
-ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.2)    #Open named port 
+ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.01)    #Open named port 
 while 1:
     try:
         data = ser.readline()
@@ -9,8 +9,8 @@ while 1:
             msg = pynmea2.parse(data)
             print(msg.lat)
             print(msg.lon)
-            time.sleep(0.2)
+            time.sleep(0.01)
     except:
         print("nothing is what it seem")
-        time.sleep(0.2)
+        time.sleep(0.01)
         pass
