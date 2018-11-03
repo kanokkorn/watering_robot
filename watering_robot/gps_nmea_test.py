2ca3108069a157ee42eeee7d31a91027b08d2ea8
Kanokkorn <kanokorn@outlook.jp>
2018-11-03 17:58:00 +0700

    not working  again

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -2,16 +2,14 @@ import serial
 import pynmea2
 
 ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.5)    #Open named port 
-try:
-    ser.open()
-    while 1:
-        try:
-            data = ser.readline()
-            if data[0:6] == '$GPGGA':                     #Set baud rate t
-                msg = pynmea2.parse(data)
-                print(msg.lat)
-                print(msg.lon)
-        except:
-            pass
-except:
-    print('nothing')
+ser.open()
+while 1:
+    try:
+        data = ser.readline()
+        if data[0:6] == '$GPGGA':                     #Set baud rate t
+            msg = pynmea2.parse(data)
+            print(msg.lat)
+            print(msg.lon)
+    except:
+        print("nothing is what it seem")
+        pass
