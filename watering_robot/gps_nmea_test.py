a313822fd72bc28b2750a7a0c071d63287a818b8
Kanokkorn <kanokorn@outlook.jp>
2018-11-08 01:16:30 +0700

    fix typo again

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -11,7 +11,7 @@ while 1:
         data = ser.readline()
         for msg in streamread.next(data):
             print(msg.lat)
-            time(0.005)
+            time.sleep(0.005)
         '''if data[0:6] == '$GPGGA':                     #Set baud rate t
             msg = pynmea2.parse(data)
             print(msg.lat)
@@ -19,10 +19,10 @@ while 1:
             time(0.005)'''
         if data[0:6] == '$GPRMC':
             print("Not what we want")
-            time(0.005)
+            time.sleep(0.005)
         elif data[0:6] == '$GPVTG':
             print("Not that too")
-            time(0.005)
+            time.sleep(0.005)
     except:
         print("nothing is what it seem")
-        time(0.005)
+        time.sleep(0.005)
