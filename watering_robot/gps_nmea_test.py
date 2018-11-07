7626d581e606a0b292dc717b142cca4542f96110
Kanokkorn <kanokorn@outlook.jp>
2018-11-08 01:10:13 +0700

    set to stream

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -3,16 +3,21 @@ import pynmea2
 import time
 
 ser = serial.Serial ("/dev/ttyS0")    #Open named port
+streamread = pynmea2.NMEAStreamReader()
 
 while 1:
+    
     try:
         data = ser.readline()
-        if data[0:6] == '$GPGGA':                     #Set baud rate t
+        for msg in streamreader.next(data):
+            print(msg.lat)
+            print(msg.lon)
+        '''if data[0:6] == '$GPGGA':                     #Set baud rate t
             msg = pynmea2.parse(data)
             print(msg.lat)
             print(msg.lon)
-            time(0.005)
-        elif data[0:6] == '$GPRMC':
+            time(0.005)'''
+        if data[0:6] == '$GPRMC':
             print("Not what we want")
             time(0.005)
         elif data[0:6] == '$GPVTG':
