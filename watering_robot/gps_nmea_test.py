2addc8a72d3026ab4f8bf8ac7d7b14e2f20a6fef
Kanokkorn <kanokorn@outlook.jp>
2018-11-08 00:35:37 +0700

    no delay

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -1,6 +1,5 @@
 import serial
 import pynmea2
-import time 
 ser = serial.Serial ("/dev/ttyS0")    #Open named port 
 while 1:
     try:
@@ -9,10 +8,8 @@ while 1:
             msg = pynmea2.parse(data)
             print(msg.lat)
             print(msg.lon)
-            time.sleep(0.01)
         else:
             pass
     except:
         print("nothing is what it seem")
-        time.sleep(0.01)
         pass
