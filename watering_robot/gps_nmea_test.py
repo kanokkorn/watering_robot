14be2c3be7ad6289f9d5934ace020070b80f6991
Kanokkorn <kanokorn@outlook.jp>
2018-11-09 00:43:26 +0900

    let serial in loop

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -1,14 +1,15 @@
 import serial
 import pynmea2
 import time 
-ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.01)    #Open named port 
+    #Open named port 
 while 1:
     try:
+        ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.01)
+        datastream = pynmea2.NMEAStreamReader()
         data = ser.readline()
         if data[0:6] == '$GPGGA':                     #Set baud rate t
             msg = pynmea2.parse(data)
-            print(msg.lat)
-            print(msg.lon)
+            print(msg.lat, msg.lon)
             time.sleep(0.01)
     except:
         print("nothing is what it seem")
