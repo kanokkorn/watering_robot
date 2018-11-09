eca5d6164c52cae2ae312552d90c52fbbc35fcae
Kanokkorn <kanokorn@outlook.jp>
2018-11-10 00:49:30 +0700

    remove try & except

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -4,17 +4,13 @@ import time
 
 while 1:
     print("Start testing ...")
-    try:
-        ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.005)
-        datastream = pynmea2.NMEAStreamReader()
-        data = ser.readline()
-        if data[0:6] == '$GPGGA':                    
-            msg = pynmea2.parse(data)
-            print(msg.lat, msg.lon)
-            time.sleep(0.01)
-        elif data[0:6] == '$GPTXT':                    
-            print("that's not it")
-            time.sleep(0.01)
-    except:
-        print("nothing")
-        time.sleep(0.005)
+    ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.005)
+    datastream = pynmea2.NMEAStreamReader()
+    data = ser.readline()
+    if data[0:6] == '$GPGGA':                    
+        msg = pynmea2.parse(data)
+        print(msg.lat, msg.lon)
+        time.sleep(0.01)
+    elif data[0:6] == '$GPTXT':                    
+        print("that's not it")
+        time.sleep(0.01)
