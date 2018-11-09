eb9c8c451e35d2ab40fc7fda203734a0be2704b6
Kanokkorn <kanokorn@outlook.jp>
2018-11-10 00:50:20 +0700

    set sleep = 1s

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -4,13 +4,13 @@ import time
 
 while 1:
     print("Start testing ...")
-    ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 0.005)
+    ser = serial.Serial ("/dev/ttyS0", 9600, timeout = 1)
     datastream = pynmea2.NMEAStreamReader()
     data = ser.readline()
     if data[0:6] == '$GPGGA':                    
         msg = pynmea2.parse(data)
         print(msg.lat, msg.lon)
-        time.sleep(0.01)
+        time.sleep(1)
     elif data[0:6] == '$GPTXT':                    
         print("that's not it")
-        time.sleep(0.01)
+        time.sleep(1)
