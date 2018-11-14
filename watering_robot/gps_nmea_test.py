d5587b21c734d31f0747160c108d72b9d2f054a6
Kanokkorn <kanokorn@outlook.jp>
2018-11-15 03:45:22 +0700

    change and to or

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -1,13 +1,19 @@
 from gps3 import gps3
+import serial
+
+ser = serial.Serial('/dev/tty.usbserial', 9600)
 gps_socket = gps3.GPSDSocket()
 data_stream = gps3.DataStream()
 gps_socket.connect()
 gps_socket.watch()
+
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
         print('Altitude = ', data_stream.TPV['lat'], 'Latitude = ', data_stream.TPV['lon'])
-        if (data_stream.TPV['lat'] != '10.725359') & (data_stream.TPV['lon'] != '99.375334'):
+        if (data_stream.TPV['lat'] != '10.725359') or (data_stream.TPV['lon'] != '99.375334'):
             print("MOVE")
-        elif  (data_stream.TPV['lat'] == '10.725359') & (data_stream.TPV['lon'] == '99.375334'):
+            ser.write('M')
+        elif  (data_stream.TPV['lat'] == '10.725359') or (data_stream.TPV['lon'] == '99.375334'):
             print("STOP")
+            ser.write('S')
