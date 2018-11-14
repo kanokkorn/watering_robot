5491fb6f546cc3e0bc144b2855fd1ef3def3a4e3
Kanokkorn <kanokorn@outlook.jp>
2018-11-15 04:31:01 +0700

    fix

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -11,10 +11,10 @@ for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
         print('Altitude = ', data_stream.TPV['lat'], 'Latitude = ', data_stream.TPV['lon'])
-        if (data_stream.TPV['lat'] != '10.725') or (data_stream.TPV['lon'] != '99.375'):
+        if (data_stream.TPV['lat'] != '10.72543') or (data_stream.TPV['lon'] != '99.375431'):
             print("MOVE")
             ser.write(str.encode('M'))
-        elif  (data_stream.TPV['lat'] == '10.725') or (data_stream.TPV['lon'] == '99.375'):
+        elif  (data_stream.TPV['lat'] == '10.72543') or (data_stream.TPV['lon'] == '99.375431'):
             print("STOP")
             ser.write(str.encode('S'))
             break
