42ac344b8a5852ae880720878dc66a1a8d73f083
Kanokkorn <kanokorn@outlook.jp>
2018-11-15 01:04:07 +0700

    fix example

--- watering_robot/gps_nmea_test.py
+++ watering_robot/gps_nmea_test.py
@@ -1,12 +1,10 @@
-import gps3
-the_connection = gps3.GPSDSocket() 
-the_fix = gps3.Fix()
-try:
-    for new_data in the_connection:
-        if new_data:
-            the_fix.refresh(new_data)
-        if not isinstance(the_fix.TPV['lat'], str): # lat as determinate of when data is 'valid'
-            speed = the_fix.TPV['speed']
-            latitude = the_fix.TPV['lat']
-            longitude = the_fix.TPV['lon']
-            altitude  = the_fix.TPV['alt']
+from gps3 import gps3
+gps_socket = gps3.GPSDSocket()
+data_stream = gps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
+for new_data in gps_socket:
+    if new_data:
+        data_stream.unpack(new_data)
+        print('Altitude = ', data_stream.TPV['alt'])
+        print('Latitude = ', data_stream.TPV['lat'])
