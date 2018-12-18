0f9db80beec39a857ba698a655535382e15ec880
Kanokkorn <kanokorn@outlook.jp>
2018-12-18 09:33:38 +0700

    bet

---
+++ watering_robot/rev.py
@@ -0,0 +1,11 @@
+import serial
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+def main():
+    return ser.write(str.encode('B'))
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Serial_STOP')
+        ser.write(str.encode('S'))
+        raise Exception('Interrupt...Program terminated.')
