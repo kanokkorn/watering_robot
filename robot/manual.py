ae3389190e53c9d54c9c0dcc7d405de7e2337c2b
Kanokkorn <kanokorn@outlook.jp>
2019-02-11 03:04:12 +0900

    keyboard control

--- robot/manual.py
+++ robot/manual.py
@@ -1,16 +1,42 @@
-import serial
+import keyboard  # using module keyboard
 import time
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-def main():
-    sig_in = str.encode(input(str("Press: ")))
-    while (sig_in != 0):
-        ser.write(sig_in)
-    else:
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+while True:  # making a loop
+    
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        
+        if keyboard.is_pressed('w'):  # if key 'q' is pressed 
+            print('You Pressed Forward !')
+            ser.write(str.encode('M'))
+            time.sleep(0.2)
+              # finishing the loop
+        
+        elif keyboard.is_pressed('s'):  # if key 'q' is pressed 
+            print('You Pressed Backward !')
+            ser.write(str.encode('B'))
+            time.sleep(0.2)
+        
+        elif keyboard.is_pressed('a'):  # if key 'q' is pressed 
+            print('You Pressed Left !')
+            ser.write(str.encode('L'))
+            time.sleep(0.2)
+            
+        elif keyboard.is_pressed('d'):  # if key 'q' is pressed 
+            print('You Pressed Right !')
+            ser.write(str.encode('R'))
+            time.sleep(0.2)
+            
+        elif keyboard.is_pressed('q'):  # if key 'q' is pressed 
+            print('You Pressed Quit !')
+            ser.write(str.encode('S'))
+            time.sleep(0.2)
+            break
+        
+        else:
+            pass
+    except:
+        
         ser.write(str.encode('S'))
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        ser.write(str.encode('S'))
-        print('Serial_STOP')
-        raise Exception('Interrupt...Program terminated.')
+        break
