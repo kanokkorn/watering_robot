4255b966ab2a844c9aed49c6b547c9050538e6d1
Kanokkorn <kanokorn@outlook.jp>
2019-03-27 01:00:52 +0700

    set range to 10

--- robot/reverse.py
+++ robot/reverse.py
@@ -2,7 +2,7 @@ import serial
 import time
 ser = serial.Serial('/dev/ttyACM0', 9600)
 def main():
-    for i in range():
+    for i in range(20):
         ser.write(str.encode('B'))
         time.sleep(1)
     ser.write(str.encode('S'))
