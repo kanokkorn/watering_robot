968da2499648354c17d1d86143cd6f9088ddf572
Kanokkorn <kanokorn@outlook.jp>
2019-04-06 21:11:22 +0700

    Add some delay before execute.

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Update and upgrade packages first" 
+echo "Update and upgrade packages first..."
+sleep 3
 apt update && apt upgrade
 cd watering_robot/robot
 python3 robot_auto.py
