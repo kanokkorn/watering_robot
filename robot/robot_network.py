970f8ff3e13888a102819b6b351c3dfbf98e6f4f
kanokkorn <kanokorn@outlook.jp>
2019-04-07 00:45:30 +0700

    Checking if network is available

--- robot/robot_network.py
+++ robot/robot_network.py
@@ -1,5 +1,15 @@
-import robot_auto
-import robot_man 
+import urllib.request
+
+def internet_on():
+    try:
+        urllib.request.urlopen('https://google.com', timeout=0.5)
+        print("Yay! Network is working normally.")
+        return True
+    except urllib.request.URLError as err:
+        raise Exception("NO! Something's wrong with network")
+        return False
 
 if __name__ == "__main__":
-    print()
+    
+    internet_on()
+    pass
