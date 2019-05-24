1fa728d18b9dac100df66f0fd5997673003da947
Kanokkorn <kanokorn@outlook.jp>
2019-05-24 14:53:11 +0700

    fomatting

--- robot/classify_edit.py
+++ robot/classify_edit.py
@@ -29,20 +29,16 @@ def read_tensor_from_image_file(
     output_name = "normalized"
     file_reader = tf.read_file(file_name, input_name)
     if file_name.endswith(".png"):
-        image_reader = tf.image.decode_png(
-            file_reader, channels=3, name="png_reader")
+        image_reader = tf.image.decode_png(file_reader, channels=3, name="png_reader")
     elif file_name.endswith(".gif"):
-        image_reader = tf.squeeze(
-            tf.image.decode_gif(file_reader, name="gif_reader"))
+        image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name="gif_reader"))
     elif file_name.endswith(".bmp"):
         image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
     else:
-        image_reader = tf.image.decode_jpeg(
-            file_reader, channels=3, name="jpeg_reader")
+        image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
     float_caster = tf.cast(image_reader, tf.float32)
     dims_expander = tf.expand_dims(float_caster, 0)
-    resized = tf.image.resize_bilinear(
-        dims_expander, [input_height, input_width])
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
     normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
     sess = tf.Session()
     result = sess.run(normalized)
@@ -85,8 +81,7 @@ if __name__ == "__main__":
 
     with tf.Session(graph=graph) as sess:
         start = time.time()
-        results = sess.run(output_operation.outputs[0], {
-                           input_operation.outputs[0]: t})
+        results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
         end = time.time()
     results = np.squeeze(results)
 
@@ -130,8 +125,7 @@ def main():
 
     with tf.Session(graph=graph) as sess:
         start = time.time()
-        results = sess.run(output_operation.outputs[0], {
-                           input_operation.outputs[0]: t})
+        results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
         end = time.time()
     results = np.squeeze(results)
 
