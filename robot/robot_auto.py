42da3a1fd0196a777b7cf52860decbd2cd2c515c
kanokkorn <kanokorn@outlook.jp>
2019-06-03 17:10:34 +0700

    Currently testing hsin function value

--- robot/robot_auto.py
+++ robot/robot_auto.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 from sys import platform
+import sys
+import robot_network
 import logging
 import argparse
 import time
 import csv
+import math
+
+file_handler = logging.FileHandler(filename="./_robot_log_.log")
+sys_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, sys_handler]
 
 logging.basicConfig(
-    filename="./_robot_log_.log",
     level=logging.DEBUG,
-    format="[%(levelname)s]:[%(name)s]: %(message)s",
+    format="[%(asctime)s.%(msecs)03d %(levelname)s]:[%(name)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=handlers,
 )
 logger = logging.getLogger("Robot-Auto")
 
 
 def arg_setup():
-    parser = argparse.ArgumentParser(description="Autonomous Robot")
-    parser.add_argument("--gps", "-g", type=str, required=True,
-                        help="GPS file format .csv")
+    parser = argparse.ArgumentParser(description="Autonomous Robot Debuging CLI")
+    parser.add_argument(
+        "--gps", "-g", type=str, required=True, help="GPS file format .csv"
+    )
     parser.add_argument(
         "--port", "-p", type=str, required=True, help="Arduino port that connect to RPi"
     )
@@ -30,14 +39,14 @@ def platform_chk():
 
     elif platform == "darwin":
 
-        logger.info("Platform detect : Linux")
+        logger.info("Platform detect : MacOS")
 
     elif platform == "win32":
 
         logger.info("Platform detect : Windows")
 
 
-def setup(port):
+def setup():
     ser = serial.Serial(port, 9600)
     gps_socket = gps3.GPSDSocket()
     data_stream = gps3.DataStream()
@@ -48,33 +57,53 @@ def setup(port):
 
 
 def test_and_carl():
-    logger.debug("Testing Sensor")
-    time.sleep(1)
-    logger.debug("Testing GPS")
-    time.sleep(1)
-    logger.debug("Test Complete")
-    time.sleep(1)
+    logger.debug("-- Testing Internet connection --")
+    robot_network.network_test()
+    time.sleep(3)
+    logger.debug("-- Checking Validate GPS data")
+    logger.info("Target " + str(csv_line()) + " checkpoints detected")
+    logger.debug("-- Testing Sensor --")
+    time.sleep(3)
+    logger.debug("-- Testing GPS --")
+    time.sleep(3)
+    logger.debug("-- Test Complete --")
     pass
 
 
 def initial():
+    for i in range(csv_line()):
+        logger.info("Checkpoint " + str(i))
+        logger.info(str(csv_read()[i][0]) + "," + str(csv_read()[i][1]))
+        distance = 10
+        test_data_a = 10.712650
+        test_data_b = 99.378680
+        while distance > 5:
+            distance = hsin(
+                test_data_a,
+                test_data_b,
+                float(csv_read()[i][0]),
+                float(csv_read()[i][1]),
+            )
+            print(distance)
+            test_data_a += 0.000005
+            test_data_b += 0.000005
+            time.sleep(0.05)
+        time.sleep(1)
+
     pass
 
 
-def read_csv():
+def csv_line():
+    file = open("./robot/test-file.csv")
+    numline = len(file.readlines())
+    return numline
+
+
+def csv_read():
     with open("./robot/test-file.csv", newline="") as pos_file:
         read = csv.reader(pos_file)
-        for gps_row in read:
-            try:
-                lat_des = float(gps_row[0])
-                lon_des = float(gps_row[1])
-                logger.info("GPS reading line...")
-                return lat_des, lon_des
-            except IndexError as err:
-                raise Exception("GPS reading error")
-                logger.error("GPS reading error")
-                break
-                
+        gps_list = list(read)
+        return gps_list
 
 
 def hsin(input_lat, input_lon, lat_des, lon_des):
@@ -98,25 +127,24 @@ def hsin(input_lat, input_lon, lat_des, lon_des):
 if __name__ == "__main__":
 
     try:
-        arg_setup()
-        logger.info("Started")
-        time.sleep(0.5)
+        logger.info("<-- Initialize -->")
         platform_chk()
         test_and_carl()
-        print(read_csv())
-        logger.info("Ended")
+        initial()
+        logger.info("<-- Terminated -->")
 
     except KeyboardInterrupt:
+        logger.critical("Keyboard interrupt!")
         raise Exception("Keyboard interrput from user. Control abort")
-        logger.error("Keyboard interrupt!")
         exit()
 else:
     try:
-        logger.info("Started")
+        logger.info("<-- Initialize -->")
         time.sleep(0.5)
         platform_chk()
         test_and_carl()
-        logger.info("Ended")
+        logger.info(str(csv_read()))
+        logger.info("<-- Terminated -->")
 
     except KeyboardInterrupt:
         raise Exception("Keyboard interrput from user. Control abort")
